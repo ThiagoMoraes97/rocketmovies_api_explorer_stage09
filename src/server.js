051fda4b78b6bd2c02dddb18c6a487e606d8531d
thiago moraes <thiagomottamoraes@gmail.com>
2024-08-14 16:43:31 -0300

    created routes and controllers folders

--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
 const express = require("express");
 
 const app = express();
+const routes = require("./routes");
+
+app.use(express.json());
+app.use(routes);
 
 const PORT = 3333;
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}.`));
+

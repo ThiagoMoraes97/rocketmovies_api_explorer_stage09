a3a0cf921f482539bfc87e4fa2720a823f249d26
thiago moraes <thiagomottamoraes@gmail.com>
2024-08-21 15:31:11 -0300

    Methods create, index, and delete for notes controller completed

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -2,6 +2,65 @@ const AppError = require("../utils/AppError");
 const knex = require("../database/knex");
 
 class NotesController {
+  async create(request, response){
+    const { title, description, rating, tags} = request.body;
+    const { user_id } = request.params;
+
+    const checkRating = rating <= 5 && rating >= 1
+
+    if(!checkRating) {
+      throw new AppError("A classificação deve ser entre 1 e 5");
+    }
+
+    const [note_id] = await knex("notes").insert({
+      title,
+      description,
+      rating,
+      user_id
+    });
+
+    const noteTags = tags.map(tag => {
+      return {
+        note_id,
+        user_id,
+        name: tag
+      }
+    });
+
+    await knex("tags").insert(noteTags);
+   
+    response.status(201).json();
+  };
+
+  async index( request, response){
+    const { user_id } = request.params;
+
+    const userNotes = await knex("notes").where({user_id}).orderBy("rating");
+    const userTags = await knex("tags").where({user_id});
+
+    const notesWithTags = userNotes.map( note => {
+      const tags = userTags.filter( tag => tag.note_id === note.id)
+      return {
+        ...note,
+        tags
+      }
+    })
+
+    response.json(notesWithTags);
+  };
+
+  async show( request, response){
+    const { id } = request.body;
+
+  };
+
+  async delete( request, response ){
+    const { id } = request.params;
+
+    await knex("notes").where({id}).delete();
+
+    response.status(204).json();
+  };
 };
 
 
--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,6 +1,7 @@
 const AppError = require("../utils/AppError");
 const { hash, compare } = require("bcryptjs");
 const knex = require("../database/knex");
+const e = require("express");
 
 class UsersController {
   async create(request, response) {
@@ -26,10 +27,45 @@ class UsersController {
   async update(request, response) {
     const {name, email, password, old_password} = request.body;
     const { id } = request.params;
+  
+    const user = await knex("users").where({id}).first();
 
-    const user = await knex("users").where({id});
+    if (email){
+      const checkEmailExist = await knex("users").where({email}).first();
+      
+      if(checkEmailExist && checkEmailExist.id !== user.id) {
+        throw new AppError("E-mail indisponível!")
+      };
+    }
 
+    if (password && !old_password) {
+      throw new AppError("É necessário passar a senha atual!")
+    };
+
+    if( password && old_password){
+
+      const checkPassword = await compare(old_password, user.password)
+     
+      if(!checkPassword) {
+        throw new AppError("Senha antiga incorreta!")
+      }
+
+      const newHashedPassword = await hash(password, 8)
+      user.password = newHashedPassword ?? user.password
+    };
+    
+    user.name = name ?? user.name
+    user.email = email ?? user.email
    
+
+    await knex("users").where({id}).update({
+      name: user.name,
+      email: user.email,
+      password: user.password
+    });
+    
+
+    return response.json();
 
   };
 

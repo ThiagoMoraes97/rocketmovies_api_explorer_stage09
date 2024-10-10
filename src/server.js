45c9f555202d9cc091d9cd4ecc5b780c65d3fba7
Thiago <thiagomottamoraes@gmail.com>
2024-10-10 18:37:27 -0300

    Environment variables and package manager installed

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 require("express-async-errors");
+require("dotenv/config");
 const express = require("express");
 
 const app = express();
@@ -6,6 +7,7 @@ const routes = require("./routes");
 const AppError = require("./utils/AppError");
 const uploadConfig = require("./configs/upload");
 const cors = require("cors");
+
 
 app.use(express.json());
 app.use(cors());
@@ -30,7 +32,7 @@ app.use((error, request, response, next) => {
   })
 });
 
-const PORT = 3333;
+const PORT = process.env.PORT || 3333;
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}.`));
 
